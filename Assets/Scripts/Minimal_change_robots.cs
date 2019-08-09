e6596320e0cae14dc161d41259374a2df7c3277a
OvercodedStack <vesperiumcreator@gmail.com>
2019-08-09 12:04:09 -0400

    Final tweaks to Vicon program
    Commented and added some minor fixes to the changing robots script

--- Assets/Scripts/Minimal_change_robots.cs
+++ Assets/Scripts/Minimal_change_robots.cs
@@ -1,21 +1,17 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using TPC_Server;
-using UnityEngine.UI;
-
 
 //A cut down version of the original Change robots script which will relay data to the
 //server exclusively. 
 public class Minimal_change_robots : MonoBehaviour
 {
-    bool change_robot;
-    float old_time;
-    TCP_Server server; 
-    public string selected_robot;
-    public raycast_collision_19 ray_caster;
-    private int selection;
-    string[] rbt_list = new string[6] { "None", "UR5", "UR10L", "UR10R", "ABBL", "ABBR" };
+    bool change_robot;                      // The boolean that allows the change of robots 
+    float old_time;                         // The old-time flag for time-keeping at a periodic time
+    TCP_Server server;                      // The primary server relay point
+    public string selected_robot;           // Which robot is being selected as the gameobject's name
+    public raycast_collision_19 ray_caster; // The raycast selector for selecting the robot
+    private int selection;                  // Which robot is being selected as an integer
+    string[] rbt_list = new string[6] { "None", "UR5", "UR10L", "UR10R", "ABBL", "ABBR" };//The following string list are the supported robots.
 
     // Use this for initialization
     void Start()
@@ -25,28 +21,31 @@ public class Minimal_change_robots : MonoBehaviour
         old_time = Time.time;
     }
 
-
     //Get the currently selected robot and send it out for relay to the server. 
     void Update()
     {
         float now_time = Time.time;
-        if (now_time > old_time + 5)
+        if (now_time > old_time + 5)// Used for periodically checking which robot is being used
         {
             try
             {
+                //Attempts to find a robot
                 selected_robot = ray_caster.get_name();
 
             }
             catch
             {
+                //If none found, default to none
                 selected_robot = "None";
             }
         }
+        //Set directly the name of the selected robot as an integer and return on the TCP server
         server.set_msg("$" + decode_str(selected_robot) + ";#\n");
     }
 
     string decode_str(string word)
     {
+        //Simply converts a string into a name and returns it.
         switch (word)
         {
             case "UR5":
@@ -63,6 +62,7 @@ public class Minimal_change_robots : MonoBehaviour
                 return "0";
         }
     }
+
     //Get the robot (manual override); 
     public string get_robot()
     {
